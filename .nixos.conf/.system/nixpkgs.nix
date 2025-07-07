a3a657ca97bff3eed7326423a41e5d7c6fefbbd3
notawyvern <218308804+notawyvern@users.noreply.github.com>
2025-07-07 19:51:45 -0300

    license, ff, flakes, rm flatpak, simpler code

---
+++ .nixos.conf/.system/nixpkgs.nix
@@ -0,0 +1,58 @@
+{ config, lib, pkgs, pkgs-unstable, ... }:
+{
+
+  users.users.crh.packages = 
+	(with pkgs; [
+		# core
+		vim
+		git
+		htop
+		efibootmgr
+		fastfetch
+
+		# desktop apps
+		virtualboxKvm
+		vscodium-fhs
+		upscayl
+		localsend
+		haruna
+		qalculate-gtk
+		kdePackages.kolourpaint 
+])
+		++
+
+	(with pkgs-unstable; [
+		ruffle
+		freetube
+		harmony-music
+]);
+
+  /* disable the nscd service,
+  aimed at servers and not desktops */
+  services.nscd.enable = false;
+  system.nssModules = lib.mkForce [];
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+ 
+  # Automatic updates
+  system.autoUpgrade = {
+	enable = true;
+	dates = "weekly";
+	};
+
+  # Automatic cleanup
+  nix.gc.automatic = true;
+  nix.gc.dates = "daily";
+  nix.gc.options = "--delete-older-than 10d";
+  nix.settings.auto-optimise-store = true; 
+
+  # enables flakes
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # Safer to leave value at the first install's version number. 
+  # Read docs before changing it ( eg. man configuration.nix )
+  system.stateVersion = "25.05"; # Did you read the comment?
+
+}
+ 
