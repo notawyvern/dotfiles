7b2ba3da93748538a6ba712b6add1ce1f51d8188
notawyvern <218308804+notawyvern@users.noreply.github.com>
2025-07-08 09:48:11 -0300

    wide changes, home mngr integrated with flakes

---
+++ .nixos.conf/nixos/flake.nix
@@ -0,0 +1,60 @@
+{
+  description = "NixOS configuration";
+
+  inputs = {
+	nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+	nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+	home-manager.url = "github:nix-community/home-manager/release-25.05";
+	home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+  outputs = { self, nixpkgs, nixpkgs-unstable, home-manager }:
+
+    let
+	system = "x86_64-linux";
+        pkgs = nixpkgs.legacyPackages.${system};
+	pkgs-unstable = import nixpkgs-unstable 
+		{inherit system; config.allowUnfree = true;};
+    in {
+
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        inherit system;
+        specialArgs = 
+          { inherit pkgs-unstable; };
+
+        modules = [
+	./hardware-configuration.nix
+
+	./.system/networking.nix
+	./.system/audio.nix
+	./.system/swap.nix
+	./.system/nixpkgs.nix
+        
+	./.core/boot.nix
+	./.core/users.nix
+	./.core/locale.nix
+
+	/*
+	valid '.nix' desktops:
+	sway, river, cosmic, gnome, xfce, openbox, 
+	lxqt, lxqt-wayland, pantheon, cinnamon
+	Pick a browser '.nix' (firefox/chromium)
+	*/
+	./.desktop/browsers/firefox.nix
+        ./.desktop/river.nix
+
+	home-manager.nixosModules.home-manager {
+
+	home-manager.useGlobalPkgs = true;
+        home-manager.useUserPackages = true;
+        
+        # modules are sourced within the home.nix dotfile
+        home-manager.users.crh = import ./.home/home.nix;
+        
+        }
+        ];
+        };
+	};
+
+}
