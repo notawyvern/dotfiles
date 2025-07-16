389bad9b9eb7b70fa8c2f77e499b2a1240d32610
notawyvern <218308804+notawyvern@users.noreply.github.com>
2025-07-16 08:48:16 -0300

    create a file for raw, non-configured packages

--- nixos/desktop/home-manager/home.nix
+++ nixos/desktop/home-manager/home.nix
@@ -1,42 +1,10 @@
-{ config, pkgs, pkgs-unstable, ... }:
+{ config, pkgs, ... }:
 
 {
   home = {
     username = "crh";
     homeDirectory = "/home/crh";
-    packages =
-      (with pkgs; [
-        # command line
-        efibootmgr
-
-        # desktop apps
-        virtualboxKvm
-        upscayl
-        localsend
-        haruna
-        qalculate-gtk
-        kdePackages.kolourpaint 
-        cosmic-files
-        cosmic-edit
-        qview
-
-        # core for river wc
-        brightnessctl
-        wayshot
-        swaybg
-	
-        # theming
-        adwaita-icon-theme
-        nerd-fonts.departure-mono 
-      ])
-          ++
-  
-      (with pkgs-unstable; [
-        # more desktop apps
-        ruffle
-        harmony-music
-      ]);
-    };
+  };
     
   xdg = {
     desktopEntries = {
