5e46c320d014474be6680fdefa97238c2af6bf79
notawyvern <218308804+notawyvern@users.noreply.github.com>
2025-07-17 18:55:57 -0300

    fix auto-updating for real + change identation

--- nixos/desktop/home-manager/hmpkgs/cli.nix
+++ nixos/desktop/home-manager/hmpkgs/cli.nix
@@ -22,31 +22,31 @@
 
   # basic configuration of git, please change to your own
   programs.git = {
-	enable = true;
-	userName = "notawyvern";
-	userEmail = "218308804+notawyvern@users.noreply.github.com";
+    enable = true;
+    userName = "notawyvern";
+    userEmail = "218308804+notawyvern@users.noreply.github.com";
   };
 
   programs.bash = {
-	enable = true;
-        enableCompletion = true;
-        sessionVariables = {
-          EDITOR = "vim";
-          VISUAL = "$EDITOR";
-        };
+    enable = true;
+    enableCompletion = true;
+    sessionVariables = {
+      EDITOR = "vim";
+      VISUAL = "$EDITOR";
+    };
     # set some aliases, feel free to add more or remove some
-	shellAliases = {
-		neofetch = "fastfetch";
-		vdir = "vdir --color=auto";
-		dir = "dir --color=auto";
-		};
-	bashrcExtra =
-        ''
-	set -o vi
-	bind -m vi-command 'Control-l: clear-screen'
-	bind -m vi-insert 'Control-l: clear-screen'
-	'';	
-      };
+    shellAliases = {
+      neofetch = "fastfetch";
+      vdir = "vdir --color=auto";
+      dir = "dir --color=auto";
+    };
+    bashrcExtra =
+      ''
+      set -o vi
+      bind -m vi-command 'Control-l: clear-screen'
+      bind -m vi-insert 'Control-l: clear-screen'
+      '';	
+  };
   
   programs.vim = {
     enable = true;
