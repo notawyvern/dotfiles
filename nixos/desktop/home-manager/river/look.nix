1d3458ba38a57342307b7e80e68d9c076d4d3db2
notawyvern <218308804+notawyvern@users.noreply.github.com>
2025-08-08 14:10:04 -0300

    make wallpaper reproducible

--- nixos/desktop/home-manager/river/look.nix
+++ nixos/desktop/home-manager/river/look.nix
@@ -13,5 +13,13 @@
       name = "Adwaita";
       package = pkgs.adwaita-icon-theme;
     };
+
+    file = {
+      "Imagens/wallpapers/wallpaper.jpeg".source =
+        pkgs.fetchurl { 
+          url = "https://i.imgur.com/WdoW1BX.jpeg";
+          sha256 = "ea8f8a05aea7db9de07bd485b1ffab5cdcb031dc6daef3f524de9c49728f9511";
+        };
+    };
   };
 }
