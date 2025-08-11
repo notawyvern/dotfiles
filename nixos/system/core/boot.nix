a0e799da86d204774eb3c52f9196dc4247613d71
notawyvern <218308804+notawyvern@users.noreply.github.com>
2025-08-11 15:48:20 -0300

    revert hack to install two versions of limine

--- nixos/system/core/boot.nix
+++ nixos/system/core/boot.nix
@@ -1,10 +1,8 @@
-{ config, pkgs, bootloader, ...}:
+{ config, pkgs, ...}:
 {
   # Bootloader.
   boot.loader.limine = {
-    enable = true; # installs a stable limine for a backup efi
-    additionalFiles =
-      { "/boot/efi/boot/DEFAULT.EFI" = "${bootloader.limine}/share/limine/BOOTX64.EFI"; };
+    enable = true;
     style = {
       wallpapers = [];
       graphicalTerminal = {
