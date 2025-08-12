6fc68ccdbfcb86110c7b5759ececdf0b9abbdeb7
notawyvern <218308804+notawyvern@users.noreply.github.com>
2025-08-12 12:20:01 -0300

    beautify flake identation

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -2,61 +2,61 @@
   description = "NixOS configuration";
 
   inputs = {
-	nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
-	nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-
-	home-manager.url = "github:nix-community/home-manager/release-25.05";
-	home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    };
-
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    
+    home-manager.url = "github:nix-community/home-manager/release-25.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+  
   outputs = { self, nixpkgs, nixpkgs-unstable, home-manager }:
-
-    let
-	system = "x86_64-linux";
-	pkgs-unstable = import nixpkgs-unstable 
-		{inherit system; config.allowUnfree = true;};
-    in {
-
+  
+  let
+    system = "x86_64-linux";
+    pkgs-unstable = import nixpkgs-unstable 
+      {inherit system; config.allowUnfree = true;};
+  in {
+    
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-        inherit system;
-        modules = [
-	./hardware-configuration.nix
+      inherit system;
+      modules = [
+        ./hardware-configuration.nix
 
-	./system/extra/networking.nix
-	./system/extra/audio.nix
-	./system/extra/locale.nix
-	./system/extra/swap.nix
+        ./system/extra/networking.nix
+        ./system/extra/audio.nix
+        ./system/extra/locale.nix
+        ./system/extra/swap.nix
         
-	./system/core/boot.nix
-	./system/core/users.nix
-	./system/core/pkgmgr.nix
+        ./system/core/boot.nix
+        ./system/core/users.nix
+        ./system/core/pkgmgr.nix
         
         ./desktop/loginmgr.nix
         ./desktop/firefox.nix
 
-	home-manager.nixosModules.home-manager {
+        home-manager.nixosModules.home-manager {
 
-	home-manager.useGlobalPkgs = true;
-        home-manager.useUserPackages = true;
-        home-manager.extraSpecialArgs = { inherit pkgs-unstable; };
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.extraSpecialArgs = { inherit pkgs-unstable; };
         
-        home-manager.users.crh.imports = [
-        ./desktop/home-manager/home.nix # create dotfiles + enable HM
+          home-manager.users.crh.imports = [
+            ./desktop/home-manager/home.nix # create dotfiles + enable HM
         
-        ./desktop/home-manager/river/look.nix
-        ./desktop/home-manager/river/river-core.nix
-        ./desktop/home-manager/river/river.nix
+            ./desktop/home-manager/river/look.nix
+            ./desktop/home-manager/river/river-core.nix
+            ./desktop/home-manager/river/river.nix
 
-        /* raw doesn't declare configurations 
-        of apps, unlike the other two */
-        ./desktop/home-manager/hmpkgs/raw.nix
-        ./desktop/home-manager/hmpkgs/cli.nix
-        ./desktop/home-manager/hmpkgs/gui.nix
+            /* raw doesn't declare configurations 
+            of apps, unlike the other two */
+            ./desktop/home-manager/hmpkgs/raw.nix
+            ./desktop/home-manager/hmpkgs/cli.nix
+            ./desktop/home-manager/hmpkgs/gui.nix
 
-        ];
+          ];
         }
-        ];
-        };
-	};
+      ];
+    };
+  };
 
 }
