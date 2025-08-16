5d5ba75e9ff5bd1d0ba733f214de6c04d6710a66
notawyvern <218308804+notawyvern@users.noreply.github.com>
2025-08-16 08:23:01 -0300

    simplify gtkgreet declaration

--- nixos/desktop/loginmgr.nix
+++ nixos/desktop/loginmgr.nix
@@ -1,18 +1,13 @@
 { config, pkgs, ... }:
 {
-  /* install the login manager for all users
-  (including root & greetd, so it works) */
-  environment.systemPackages =
-  with pkgs; [ greetd.gtkgreet cage ];
-
   services.greetd = {
 	enable = true;
 	restart = true;
 	vt = 7;
 	settings = {
 	default_session = {
-		command =
-                ''cage -d -s -- gtkgreet -c river'';
+		command = with pkgs;
+                ''${cage}/bin/cage -d -s -- ${greetd.gtkgreet}/bin/gtkgreet -c river'';
 	};
 	};
 	};
