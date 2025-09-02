cbf3ea607b592878cf8a2959813720d7c1e30251
notawyvern <218308804+notawyvern@users.noreply.github.com>
2025-09-02 20:03:37 -0300

    make firefox look like qutebrowser

--- nixos/desktop/home-manager/home.nix
+++ nixos/desktop/home-manager/home.nix
@@ -8,6 +8,27 @@
     
   xdg = {
     desktopEntries = {
+      htop = {
+        name = "Htop";
+        exec = "alacritty -e htop";
+        terminal = false;
+      };
+      firefox = {
+        name = "hckrfox";
+        exec = "firefox --name firefox %U";
+        mimeType = [ 
+          "text/html" 
+          "text/xml" 
+          "application/xhtml+xml"
+          "application/vnd.mozilla.xul+xml"
+          "x-scheme-handler/http" 
+          "x-scheme-handler/https" 
+        ];
+      };
+      vim = {
+        name = "Vim";
+        noDisplay = true;
+      };
       gvim = {
         name = "gvim";
         noDisplay = true;
