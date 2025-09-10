b792892ebc2999784853498ca11f82eb13755d4c
notawyvern <218308804+notawyvern@users.noreply.github.com>
2025-09-10 15:49:47 -0300

    move wallpapers from ~/Imagens to ~/.wallpapers

--- nixos/desktop/home-manager/river/river.nix
+++ nixos/desktop/home-manager/river/river.nix
@@ -95,7 +95,7 @@
       };
       spawn = [
         "i3bar-river"
-        "'${pkgs.swaybg}/bin/swaybg -i ~/Imagens/wallpapers/magical-land-japan.jpg'"
+        "'${pkgs.swaybg}/bin/swaybg -i ~/.wallpapers/magical-land-japan.jpg'"
       ];
     };
     extraConfig =
