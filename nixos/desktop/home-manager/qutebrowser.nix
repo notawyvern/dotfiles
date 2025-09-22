23f2150b19566044ea2cabfeae2f3c8c23bc7d8d
notawyvern <218308804+notawyvern@users.noreply.github.com>
2025-09-21 21:41:01 -0300

    quickmark listenbrainz on qutebrowser

--- nixos/desktop/home-manager/qutebrowser.nix
+++ nixos/desktop/home-manager/qutebrowser.nix
@@ -55,6 +55,7 @@
       anibunker = "https://anibunker.com/";
       betteranime = "https://betteranime.net/";
       weebcentral = "https://weebcentral.com/";
+      spotify = "https://listenbrainz.org/";
       youtube = "https://inv.nadeko.net/";
       mal = "https://myanimelist.net/animelist/weeabotaku";
       mangafire = "https://mangafire.to/";
