c3d55d27ef25c443055bc17f28c6500dc4a0d493
notawyvern <218308804+notawyvern@users.noreply.github.com>
2025-10-06 08:23:50 -0300

    reorganize dotfiles for better git control

---
+++ lxqt/desktop/home-manager/home.nix
@@ -0,0 +1,50 @@
+{ config, pkgs, ... }:
+
+{
+  home = {
+    username = "crh";
+    homeDirectory = "/home/crh";
+  };
+    
+  xdg = {
+    desktopEntries = {
+      htop = {
+        name = "Htop";
+        exec = "alacritty -e htop";
+        terminal = false;
+      };
+      vim = {
+        name = "Vim";
+        noDisplay = true;
+      };
+      gvim = {
+        name = "gvim";
+        noDisplay = true;
+      };
+    };
+    mimeApps = {
+      enable = true;
+      defaultApplications = {
+        "image/jpeg" = "swayimg.desktop";
+        "image/png" = "swayimg.desktop";
+      };
+      associations.added = {
+        "image/jpeg" = "swayimg.desktop";
+        "image/png" = "swayimg.desktop";
+      };
+      associations.removed = {
+        "image/png" = [ "org.kde.kolourpaint.desktop" "org.qutebrowser.qutebrowser.desktop" ];
+        "image/jpeg" = [ "org.kde.kolourpaint.desktop" "org.qutebrowser.qutebrowser.desktop" ];
+      };
+    };
+  };
+
+  # enables home manager
+  programs.home-manager.enable = true;
+
+  # Value of configuration compatible with Home Manager. To keep
+  # it avoids breakage due to changes.
+  #
+  # No need to change it but, if you want to, read the release notes.
+  home.stateVersion = "25.05";
+}
---
+++ lxqt/flake.nix
@@ -0,0 +1,62 @@
+{
+  description = "NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs-hold.url = "github:nixos/nixpkgs/659e1897c70b112dee2ca33c96390c4c4ae1130b";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    
+    home-manager.url = "github:nix-community/home-manager/release-25.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+  
+  outputs = { self, nixpkgs, nixpkgs-hold, nixpkgs-unstable, home-manager }:
+  
+  let
+    system = "x86_64-linux";
+    pkgs-hold = import nixpkgs-hold {inherit system;};
+    pkgs-unstable = import nixpkgs-unstable 
+      {inherit system; config.allowUnfree = true;};
+  in {
+    
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      inherit system;
+      specialArgs = { inherit pkgs-hold; };
+      modules = [
+        ./hardware-configuration.nix
+
+        ./system/extra/networking.nix
+        ./system/extra/audio.nix
+        ./system/extra/locale.nix
+        ./system/extra/swap.nix
+        
+        ./system/core/boot.nix
+        ./system/core/users.nix
+        ./system/core/pkgmgr.nix
+
+        ./desktop/lxqt.nix # enables de and login manager
+
+        home-manager.nixosModules.home-manager {
+
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.extraSpecialArgs = { inherit pkgs-unstable; inherit pkgs-hold; };
+        
+          home-manager.users.crh.imports = [
+            ./desktop/home-manager/home.nix # create dotfiles + enable HM
+            ./desktop/home-manager/look.nix
+            ./desktop/home-manager/firefox.nix
+        
+            /* raw doesn't declare configurations 
+            of apps, unlike the other two */
+            ./desktop/home-manager/hmpkgs/raw.nix
+            ./desktop/home-manager/hmpkgs/cli.nix
+            ./desktop/home-manager/hmpkgs/gui.nix
+
+          ];
+        }
+      ];
+    };
+  };
+
+}
