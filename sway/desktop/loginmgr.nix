8d0223e46b7e337d3be7c33fd700e23eaaf31cb0
notawyvern <218308804+notawyvern@users.noreply.github.com>
2025-10-12 12:05:53 -0300

    cleaner code for launching compositor in greetd

--- sway/desktop/loginmgr.nix
+++ sway/desktop/loginmgr.nix
@@ -1,17 +1,15 @@
 { config, pkgs, ... }:
 {
-  /* the global install prevents an attempt to 
-  use unseen features nonstop that lag sway's 
-  and gtkgreet's loading */
   programs.sway = {
     enable = true;
+    package = null;
     extraPackages = [ ];
   };
 
   environment.etc."greetd/sway.conf" = {
     text = with pkgs;
     ''
-    exec "${greetd.gtkgreet}/bin/gtkgreet -l -c ${sway}/bin/sway; swaymsg exit"
+    exec "${greetd.gtkgreet}/bin/gtkgreet -l -c ${sway}/bin/sway; ${sway-unwrapped}/bin/swaymsg exit"
     include /etc/sway/config.d/*
     input type:touchpad { 
       tap enabled 
